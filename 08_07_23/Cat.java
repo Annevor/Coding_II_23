4f00b51e9007d80f94cfdc4e22d4a78d305f8291
Annevor <141165868+Annevor@users.noreply.github.com>
2023-08-08 16:07:25 +0000

    Made a neat little smiley guy. He's pretty cool.

---
+++ 08_07_23/Cat.java
@@ -0,0 +1,15 @@
+public class Cat{
+    //Properties (fields)
+    public String  name = "Gorlock";
+    public int  age = 732;
+    public String  breed = "Cat(?)";
+    public String  food = "Person";
+
+    //Behaviors (methods)
+    public void displayCat(){
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Breed: " + breed);
+        System.out.println("Favorite Food: " + food);
+    }// end of display method //
+}
