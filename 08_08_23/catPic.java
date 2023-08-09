1b6c3d9c24602fd84f982317a8d456a1868ae29c
Annevor <141165868+Annevor@users.noreply.github.com>
2023-08-09 06:42:13 +0000

    This commit hopefully works

--- 08_08_23/catPic.java
+++ 08_08_23/catPic.java
@@ -1,12 +1,12 @@
 public class catPic {
     public static void main(String[] args){
-        System.out.println(" \u2588");
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
+        System.out.println(" |>------<| ");
+        System.out.println(" \\0\\    /0/ ");
+        System.out.println("  \\/    \\/  ");
+        System.out.println("\\    ..    /");
+        System.out.println("||  <-->  ||");
+        System.out.println("||   ||   ||");
+        System.out.println(" \\\\  ||  // ");
+        System.out.println("  \\_/  \\_/");
     }
 }
