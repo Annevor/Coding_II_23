d3bf22eed6340af7ede161a22fa651ea6fd11856
Annevor <141165868+Annevor@users.noreply.github.com>
2023-08-09 06:37:31 +0000

    this has the sick cat pic

---
+++ 08_08_23/catPic.java
@@ -0,0 +1,12 @@
+public class catPic {
+    public static void main(String[] args){
+        System.out.println(" \u2588");
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+}
