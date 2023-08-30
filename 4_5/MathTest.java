d28ca2c0439d6126ce105b1681d88c9159a1e2a3
Annevor <141165868+Annevor@users.noreply.github.com>
2023-08-30 16:44:30 +0000

    The math class IN MATH CLASS

---
+++ 4_5/MathTest.java
@@ -0,0 +1,12 @@
+public class MathTest {
+    public static void main(String[] args) {
+        double one = Math.abs(-1.23);
+        double two = Math.pow(3,2);
+        double three = Math.sqrt(121.0) - Math.sqrt(256.0);
+        double four = Math.abs(Math.min(-3,-5));
+        System.out.println(one);
+        System.out.println(two);
+        System.out.println(three);
+        System.out.println(four);
+    }
+}
