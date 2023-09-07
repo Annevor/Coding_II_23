690c877f590a2f4a26fcca0307bf639646ed4c10
Annevor <141165868+Annevor@users.noreply.github.com>
2023-09-07 16:25:05 +0000

    infinite loop

--- Euler_Projects/Fibonacci.java
+++ Euler_Projects/Fibonacci.java
@@ -5,11 +5,11 @@ public class Fibonacci{
         System.out.print("Enter number of desired terms for Fibonacci numbers: ");
         int terms = sc.nextInt();
         sc.close();
-        long fib[] = {0L, 1L, 2L};
-        for(int i = 1; i <= terms; i++){
-            fib[2] = fib[1] + fib[0];
-            fib[0] = fib[1];
-            fib[1] = fib[2];
+        long fib[] = {0L, 1L, 2L};        // first three terms of Fibonacci
+        for(int i = 0; i < terms; i++){
+            fib[2] = fib[1] + fib[0];     // reassigning of third term in the list
+            fib[0] = fib[1];              // memory shift to the left; reassigning first term with second
+            fib[1] = fib[2];              // further shift; reassigning second term with third; loop reiterates
             System.out.println(fib[1]);
         }
     }
