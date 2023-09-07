2e08f86ce81615caba463d9abfc12d79cacc676e
Annevor <141165868+Annevor@users.noreply.github.com>
2023-09-07 15:43:27 +0000

    ?

--- Euler_Projects/Fibonacci.java
+++ Euler_Projects/Fibonacci.java
@@ -4,13 +4,13 @@ public class Fibonacci{
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number of desired terms for Fibonacci numbers: ");
         int terms = sc.nextInt();
-        long sequence[] = {0L, 1L, 2L};
-        for(int i = 1; i <= terms; i++){
-            sequence[2] = sequence[1] + sequence[0];
-            sequence[0] = sequence[1];
-            sequence[1] = sequence[2];
-            System.out.println(sequence[1]);
-        }
         sc.close();
+        long fib[] = {0L, 1L, 2L};
+        for(int i = 1; i <= terms; i++){
+            fib[2] = fib[1] + fib[0];
+            fib[0] = fib[1];
+            fib[1] = fib[2];
+            System.out.println(fib[1]);
+        }
     }
 }
